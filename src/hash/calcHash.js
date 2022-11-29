b99e9842c5880c1b6799ce167f2a1e9af4d23a9f
maxim545 <maxximc95@gmail.com>
2022-11-29 23:15:13 +0300

    feat: add solution for hash task

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,17 @@
+import crypto from 'crypto';
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const pathToFile = path.join(__dirname, 'files', 'fileToCalculateHashFor.txt');
+
 const calculateHash = async () => {
-    // Write your code here 
+    const dataFromFile = await fs.promises.readFile(pathToFile, { encoding: 'utf8' })
+    const hash = crypto.createHash('sha256').update(dataFromFile).digest('hex');
+    console.log(hash)
 };
 
 await calculateHash();
