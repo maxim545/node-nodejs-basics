fd4f7a5702cd2fd66dc80f1409287c297a827c7c
maxim545 <maxximc95@gmail.com>
2022-11-29 17:40:25 +0300

    feat: add solution for cli tasks

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,10 @@
 const parseArgs = () => {
-    // Write your code here 
+    let allArgs = process.argv.filter(arg => arg.indexOf('--') === 0);
+    allArgs = allArgs.map(arg => {
+        const currentIndex = process.argv.findIndex(item => item === arg);
+        return arg.slice(2) + ' is ' + process.argv[currentIndex + 1];
+    });
+    console.log(allArgs.join(', '));
 };
 
 parseArgs();
