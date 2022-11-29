f55fe4f4c889c7f444b66456f9e5e187417280a6
maxim545 <maxximc95@gmail.com>
2022-11-29 22:17:39 +0300

    refactor: remove unnecessary import

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,10 +1,9 @@
 import fs from 'fs';
 import path from 'path';
 import { fileURLToPath } from 'url';
-import { dirname } from 'path';
 
 const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+const __dirname = path.dirname(__filename);
 
 const pathTo = path.join(__dirname, 'files', 'fileToRead.txt');
 
