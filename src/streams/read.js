634f8d15c0f1244a7e9d5feb0a81e63b3aefc92c
maxim545 <maxximc95@gmail.com>
2022-11-30 14:37:37 +0300

    feat: add solution for streams tasks

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,17 @@
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const pathToFile = path.join(__dirname, 'files', 'fileToRead.txt');
+
 const read = async () => {
-    // Write your code here 
+    const readStream = fs.createReadStream(pathToFile, 'utf-8');
+    let str = '';
+    readStream.on('data', chunk => str += chunk);
+    readStream.on('end', () => process.stdout.write(str));
 };
 
 await read();
--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,13 @@
+import { Transform } from "stream";
+
 const transform = async () => {
-    // Write your code here 
+    const reverseStr = new Transform({
+        transform(chunk, encoding, callback) {
+            this.push(chunk.toString().split('').reverse().join(''));
+            callback();
+        }
+    })
+    process.stdin.pipe(reverseStr).pipe(process.stdout);
 };
 
 await transform();
