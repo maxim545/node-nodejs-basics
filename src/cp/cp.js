45899896a5de6cab4d053639b0d37541efbef5d1
maxim545 <maxximc95@gmail.com>
2022-12-05 17:54:13 +0300

    fix: add correct solution for cp task

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -8,9 +8,21 @@ const __dirname = path.dirname(__filename);
 const pathToFile = path.join(__dirname, 'files', 'script.js');
 
 const spawnChildProcess = async (args) => {
-    child_process.fork(pathToFile, [...args], {
-        stdio: [0, 1, 2, 'ipc'],
+    const cp = child_process.spawn('node', [pathToFile, ...args]);
+    process.stdin.pipe(cp.stdin)
+
+    cp.stdout.on('data', (data) => {
+        console.log(`${data}`);
     });
+
+    cp.stderr.on('data', (data) => {
+        console.error(`${data}`);
+    });
+
+    cp.on('close', (code) => {
+        console.log(`child process exited with code ${code}`);
+    });
+
 };
 
 spawnChildProcess(process.argv);
